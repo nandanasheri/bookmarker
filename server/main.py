22f148569a67bebf2cb6fad455e0b9e3f3fdfd03
Nandana <nandanasheri@gmail.com>
2025-08-06 18:32:33 -0500

    scraping website url and set up pinceone index

--- server/main.py
+++ server/main.py
@@ -1,6 +1,29 @@
 from flask import Flask
-
+from pinecone import Pinecone
+import os
 app = Flask(__name__)
+from dotenv import load_dotenv
+
+# create a pinceone client to interact with vector db
+load_dotenv()
+pinecone_api = os.getenv("PINECONE_API_KEY")
+# Pinecone client initialization
+pc = Pinecone(api_key=pinecone_api)
+
+# Create a dense index with integrated embedding
+def create_dense_index():
+    index_name = "dense_bookmarks"
+    if not pc.has_index(index_name):
+        pc.create_index_for_model(
+            name=index_name,
+            cloud="aws",
+            region="us-east-1",
+            embed={
+                "model":"llama-text-embed-v2",
+                "field_map":{"text": "chunk_text"}
+            }
+        )
+
 
 @app.route("/")
 def hello_world():
---
+++ server/test.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+from urllib.request import urlopen
+
+# if it's server side rendered you don't need playwright to scrape you can directly use urllib
+def plain_html_scrape():
+    link = "https://github.com/nandanasheri"
+    f = urlopen(link)
+    content = f.read()
+    return content
+
+# if client side rendered, requires playwright to execute javascript to get content of the page
+def js_scrape():
+    with sync_playwright() as pw:
+        # create browser instance
+        browser = pw.chromium.launch(
+            # Headless mode so it doesn't open a GUI
+            headless=True,
+        )
+        # create context
+        context = browser.new_context(viewport={"width": 1920, "height": 1080})
+        page = context.new_page()
+
+        # go to url
+        page.goto("https://v1.nandana.dev")
+        # get HTML
+        return page.content()
+
+def chunk_text(content):
+    soup = BeautifulSoup(content, 'html.parser')
+     # Remove noisy tags
+    for tag in soup(["script", "style", "noscript"]):
+        tag.decompose()
+
+    for tag in soup.find_all(["nav", "footer"]):
+        tag.decompose()
+    
+    print(soup.get_text())
+    
+text = plain_html_scrape()
+chunk_text(text)
