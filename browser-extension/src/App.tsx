4a78fee474ca66e3253dc1700338c3bb7b455c0d
Nandana <nandanasheri@gmail.com>
2025-08-12 17:11:25 -0500

    fix: working on content script for URLs

--- browser-extension/src/App.tsx
+++ browser-extension/src/App.tsx
@@ -1,8 +1,30 @@
+
 function App() {
+
+  function getPageInfo() {
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tabId = tabs[0].id;
+    
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError.message);
+      return;
+    }
+
+    // Send message to content script
+    if (tabId !== undefined) {
+      chrome.tabs.sendMessage(tabId, { type: "GET_PAGE_DATA" }, (response) => {
+        console.log("Page info from content.ts:", response);
+      });
+    }
+  });
+}
 
   return (
     <>
-      <h1>My Browser Extension</h1>
+      <h2>Bookmark this Page</h2>
+      <input type="text" id="title" placeholder="Enter Notes" />
+      <button id="bookmarkPage" onClick={getPageInfo}>Bookmark this Page</button>
     </>
   )
 }
